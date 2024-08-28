4e834334ec2018a5636ec8e66dd800eab15dfadc
FabioDiCeglie <fabiod@backbase.com>
2024-08-28 17:08:53 +0200

    feat: working on CustomFormField and created a render field component

--- components/forms/CustomFormField.tsx
+++ components/forms/CustomFormField.tsx
@@ -2,7 +2,6 @@
 
 import {
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -10,24 +9,69 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Control } from 'react-hook-form';
+import { FormFieldType } from './PatientForm';
+import Image from 'next/image';
 
 interface CustomProps {
-    control: Control<any>
+  control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
 }
 
-const CustomFormField = ({ control }: CustomProps) => {
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { fieldType, placeholder, iconSrc, iconAlt } = props;
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className='flex rounded-md border border-dark-500 bg-dark-400'>
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              alt={iconAlt || 'icon'}
+              height={24}
+              width={24}
+              className='ml-2'
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className='shad-input border-0'
+            />
+          </FormControl>
+        </div>
+      );
+    case FormFieldType.TEXTAREA:
+      break;
+  }
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label, placeholder, iconSrc, iconAlt } =
+    props;
   return (
     <FormField
       control={control}
-      name='username'
+      name={name}
       render={({ field }) => (
-        <FormItem>
-          <FormLabel>Username</FormLabel>
-          <FormControl>
-            <Input placeholder='Username' {...field} />
-          </FormControl>
-          <FormDescription>This is your public display name.</FormDescription>
-          <FormMessage />
+        <FormItem className='flex-1'>
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+
+          <RenderField field={field} props={props} />
+
+          <FormMessage className='shad-error' />
         </FormItem>
       )}
     />
